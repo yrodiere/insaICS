4f07af25847a5b32329e2475ef8c1e1e59bd1159
fenrhil <fenrhil@gmail.com>
2010-12-06 22:56:13 +0100

    Copyright notices

--- insaICS.sh
+++ insaICS.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+#
 #### insaICS.sh
-## Process INSA-J.F. Petit's DOS ICS files, by converting them
+## Process INSA-IF DOS ICS files, by converting them
 ## to a proper file format and summing up the events' descriptions
 #### DEPENDENCIES
 ## see COMMANDS below
